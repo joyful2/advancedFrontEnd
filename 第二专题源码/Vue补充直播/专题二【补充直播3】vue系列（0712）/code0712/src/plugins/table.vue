75e72a2c69d81f833475a3943fbbdf811ab659b1
y_cx <y_cx@winning.com.cn>
2022-11-03 23:15:49 +0800

    update

--- 第二专题源码/Vue补充直播/专题二【补充直播3】vue系列（0712）/code0712/src/plugins/table.vue
+++ 第二专题源码/Vue补充直播/专题二【补充直播3】vue系列（0712）/code0712/src/plugins/table.vue
@@ -19,6 +19,22 @@
 export default{
         name:"v-table",
         props:{
+        //     tableData:[
+        //     {'a':'1','b':'2','c':'3','d':'8'},
+        //     {'a':'4','b':'5','c':'6','d':'9'}
+        // ],
+        // thLabel:[
+        //     [
+        //         {prop:'a',label:'th[1][1]',rowspan:'2'},
+        //         {label:'th[1][2]',colspan:'2'},
+        //         {label:'th[1][3]'}
+        //     ],
+        //     [
+        //         {prop:'c',label:'th[2][1]'},
+        //         {prop:'b',label:'th[2][2]'},
+        //         {prop:'d',label:'th[2][3]'}
+        //     ]
+        // ]
             datas:{ //内容数组
                 type:Array
             },
@@ -53,6 +69,7 @@ export default{
                 for(let i = 0,len = thLabel.length; i<len; i++){    
                     for(let j  = 0; j < thLabel[i].length; j++){
                         for (const key in thLabel[i][j]) {
+                            // todo rowspan 和 colspan 是table 标签原生支持的属性 ！
                            key == 'rowspan' && thTr[i].childNodes[j].setAttribute('rowspan',thLabel[i][j][key]);
                            key == 'colspan' && thTr[i].childNodes[j].setAttribute('colspan',thLabel[i][j][key]);
                         }
